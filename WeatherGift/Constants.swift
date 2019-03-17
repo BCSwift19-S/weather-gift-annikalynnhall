8d2ddc6394f644ad2daca25ab246862ca0ea689a
Annika <annikahall@annikas-macbook-pro.local>
2019-03-17 15:42:20 -0400

    2.15 Completed: Alamofire/Dark Sky API

---
+++ WeatherGift/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  WeatherGift
+//
+//  Created by Annika Lynn Nordstrom Hall on 3/17/19.
+//  Copyright © 2019 Annika Hall. All rights reserved.
+//
+
+import Foundation
+
+let urlBase = "https://api.darksky.net/forecast/"
+let urlAPIKey = "0b7e46066fde89b30e2bd2fb93ec3c6f/"
--- WeatherGift/DetailVC.swift
+++ WeatherGift/DetailVC.swift
@@ -77,7 +77,7 @@ extension DetailVC: CLLocationManagerDelegate {
         currentLocation = locations.last
         let currentLatitude = currentLocation.coordinate.latitude
         let currentLongitude = currentLocation.coordinate.longitude
-        let currentCoordinates = "\(currentLatitude), \(currentLongitude)"
+        let currentCoordinates = "\(currentLatitude),\(currentLongitude)"
         dateLabel.text = currentCoordinates
         geoCoder.reverseGeocodeLocation(currentLocation, completionHandler:
             {placemarks, error in
@@ -90,6 +90,7 @@ extension DetailVC: CLLocationManagerDelegate {
             }
             self.locationsArray[0].name = place
             self.locationsArray[0].coordinates = currentCoordinates
+            self.locationsArray[0].getWeather()
             self.updateUserInterface()
         })
     }
